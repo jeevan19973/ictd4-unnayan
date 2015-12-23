12ae3650bc3a73c74353cf4bb7e3427594e1c31b
BlazingFire <abhi.jaiswal44@gmail.com>
2015-12-23 13:59:03 +0530

    Add images for skills to make things easier for villagers

---
+++ Job/Users/UserJobProfile.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="UserJobProfile.aspx.cs" Inherits="Job_Users_UserJobProfile" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-large;
+        }
+
+        
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="text-align: center" class="auto-style1">
+            Welcome 
+            <asp:Label ID="Label_Username" runat="server" Style="font-size: large; font-weight: 700"></asp:Label>
+            
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_Logout" runat="server" Text="logout" OnClick="Button_Logout_Click" style="text-align: right" />
+
+            <br />
+
+        </div>
+
+        <div class="auto-style1">
+            <strong style="text-align: left">Work you can do 
+            </strong>
+            <br />
+            <asp:Label ID="Label_SkillsSet" runat="server"></asp:Label>
+            <br />
+            <br />
+        </div>
+        <div class="auto-style1">
+            <strong>Select an image&nbsp;Or click on the name to add a SKILL to your known Work list</strong><br />
+            <br />
+        </div>
+        <div>
+            <asp:ImageButton ID="ImageButton_Fishing" runat="server" Height="150px" ImageUrl="~/images/Job_Skills/download.jpg" Width="150px" OnClick="Button_Fishing_Click" />
+            <asp:ImageButton ID="ImageButton_Handicraft" runat="server" Height="150px" ImageUrl="~/images/Job_Skills/images.jpg" Width="150px" OnClick="Button_Handicraft_Click" />
+            <asp:ImageButton ID="ImageButton_Farming" runat="server" Height="150px" ImageUrl="~/images/Job_Skills/indian-farmer.jpg" Width="150px" OnClick="Button_Farming_Click" />
+            <asp:ImageButton ID="ImageButton_Teaching" runat="server" Height="150px" ImageUrl="~/images/Job_Skills/Teaching_in_Indian_Village_Schools_1.jpg" Width="150px" OnClick="Button_Teaching_Click" />
+            <asp:ImageButton ID="ImageButton_Weaving" runat="server" Height="150px" ImageUrl="~/images/Job_Skills/Weaving_in_Indian_Villages.jpg" Width="150px" OnClick="Button_Weaving_Click" />
+
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_Fishing" runat="server" OnClick="Button_Fishing_Click" Text="Fishing" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_Handicraft" runat="server" Text="Handicraft" OnClick="Button_Handicraft_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_Farming" runat="server" Text="Farming" OnClick="Button_Farming_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_Teaching" runat="server" Text="Teaching" OnClick="Button_Teaching_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_Weaving" runat="server" Text="Weaving" OnClick="Button_Weaving_Click" />
+            <br />
+        </div>
+
+        <asp:Label ID="Label_AlreadyExists" runat="server" Text="The skill you have added already exists in your skill set" Visible="False"></asp:Label>
+
+    </form>
+</body>
+</html>
---
+++ Job/Users/UserJobProfile.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Job_Users_UserJobProfile : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["Username"] != null)
+            Label_Username.Text = Session["Username"].ToString();
+        else
+            Response.Redirect("Login_FirstPage.aspx");
+        if (!IsPostBack)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Job_Registration_ConnectionString"].ConnectionString);
+            SqlDataReader rdr = null;
+            try
+            {
+                String select_query = "SELECT SKill FROM Skills where Username = @Usn";
+                conn.Open();
+                SqlCommand comm = new SqlCommand(select_query, conn);
+                comm.Parameters.AddWithValue("@Usn", Session["Username"].ToString());
+                rdr = comm.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Label_SkillsSet.Text = Label_SkillsSet.Text + rdr["Skill"].ToString() + " ";
+                }
+
+            }
+            catch (Exception insertError)
+            {
+                Response.Write(insertError.ToString());
+            }
+            finally
+            {
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+                if (conn != null)
+                    conn.Close();
+            }
+        }
+    }
+
+    protected void Button_Fishing_Click(object sender, EventArgs e)
+    {
+        String skill = "Fishing";
+        addSkillTo_SkillsDatabase(skill);
+        
+
+    }
+    protected void Button_Handicraft_Click(object sender, EventArgs e)
+    {
+        String skill = "Handicraft";
+        addSkillTo_SkillsDatabase(skill);
+    }
+    protected void Button_Farming_Click(object sender, EventArgs e)
+    {
+        String skill = "Farming";
+        addSkillTo_SkillsDatabase(skill);
+    }
+    protected void Button_Teaching_Click(object sender, EventArgs e)
+    {
+        String skill = "Teaching";
+        addSkillTo_SkillsDatabase(skill);
+    }
+    protected void Button_Weaving_Click(object sender, EventArgs e)
+    {
+        String skill = "Weaving";
+        addSkillTo_SkillsDatabase(skill);
+    }
+    protected void addSkillTo_SkillsDatabase(String skill)
+    {
+        Label_AlreadyExists.Visible = false;
+        if (Label_SkillsSet.Text.Contains(skill))
+        {
+            Label_AlreadyExists.Visible = true;
+            return;
+        }
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Job_Registration_ConnectionString"].ConnectionString);
+        try
+        {
+            String insert_query = "INSERT into Skills VALUES (@Usn,@Skill)";
+            conn.Open();
+            SqlCommand comm = new SqlCommand(insert_query, conn);
+            comm.Parameters.AddWithValue("@Usn", Session["Username"].ToString());
+            comm.Parameters.AddWithValue("@Skill", skill);
+            comm.ExecuteNonQuery();
+        }
+        catch (Exception insertError)
+        {
+            Response.Write(insertError.ToString());
+        }
+        finally
+        {
+
+            if (conn != null)
+                conn.Close();
+            Label_SkillsSet.Text = Label_SkillsSet.Text + skill + " ";
+        }
+    }
+    protected void Button_Logout_Click(object sender, EventArgs e)
+    {
+        Session["Username"] = null;
+        Response.Redirect("Login_FirstPage.aspx");
+    }
+    
+}
