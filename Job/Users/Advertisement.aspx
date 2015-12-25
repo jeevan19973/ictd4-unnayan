e6e1fa0cf2d4322d4be6abe512ccc1868bfc926f
BlazingFire <abhi.jaiswal44@gmail.com>
2015-12-25 21:51:57 +0530

    Show various advertisements here, use images of skills so that villagers can search for the advertisement for that particular skill

---
+++ Job/Users/Advertisement.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Advertisement.aspx.cs" Inherits="Job_Users_Advertisement" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-large;
+            text-align: center;
+        }
+        .auto-style2 {
+            font-size: x-large;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="auto-style1">
+    
+        <strong>Advertisements for various jobs -</strong></div>
+    <p >
+        <asp:GridView ID="GridView_Advertisements" runat="server" >
+        </asp:GridView>
+        </p>
+        <p class="auto-style2" >
+            <strong>Select the skill you have to see advertisement for that skill</strong></p>
+        <div>
+            <asp:ImageButton ID="ImageButton_Fishing" runat="server" Height="150px" ImageUrl="~/images/Job_Skills/download.jpg" Width="150px" OnClick="Button_Fishing_Click" />
+            <asp:ImageButton ID="ImageButton_Handicraft" runat="server" Height="150px" ImageUrl="~/images/Job_Skills/images.jpg" Width="150px" OnClick="Button_Handicraft_Click" />
+            <asp:ImageButton ID="ImageButton_Farming" runat="server" Height="150px" ImageUrl="~/images/Job_Skills/indian-farmer.jpg" Width="150px" OnClick="Button_Farming_Click" />
+            <asp:ImageButton ID="ImageButton_Teaching" runat="server" Height="150px" ImageUrl="~/images/Job_Skills/Teaching_in_Indian_Village_Schools_1.jpg" Width="150px" OnClick="Button_Teaching_Click" />
+            <asp:ImageButton ID="ImageButton_Weaving" runat="server" Height="150px" ImageUrl="~/images/Job_Skills/Weaving_in_Indian_Villages.jpg" Width="150px" OnClick="Button_Weaving_Click" />
+
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_Fishing" runat="server" OnClick="Button_Fishing_Click" Text="Fishing" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_Handicraft" runat="server" Text="Handicraft" OnClick="Button_Handicraft_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_Farming" runat="server" Text="Farming" OnClick="Button_Farming_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_Teaching" runat="server" Text="Teaching" OnClick="Button_Teaching_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_Weaving" runat="server" Text="Weaving" OnClick="Button_Weaving_Click" />
+            <br />
+        </div>
+        <p>
+    </form>
+    </body>
+</html>
---
+++ Job/Users/Advertisement.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public partial class Job_Users_Advertisement : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Job_Registration_ConnectionString"].ConnectionString);
+            try
+            {
+                //String choose_query = "Select Username FROM Skills where skill = @Skill";
+                String select_query = "SELECT * FROM Advertisers";
+                conn.Open();
+                SqlCommand comm = new SqlCommand(select_query, conn);
+
+                GridView_Advertisements.DataSource = comm.ExecuteReader();
+                GridView_Advertisements.DataBind();
+
+            }
+            catch (Exception insertError)
+            {
+                Response.Write(insertError.ToString());
+            }
+            finally
+            {
+
+                if (conn != null)
+                    conn.Close();
+
+            }
+        }
+
+    }
+
+    protected void Button_Fishing_Click(object sender, EventArgs e)
+    {
+        String skill = "Fishing";
+        findAdvertiserfrom_AdvertisersDatabase(skill);
+
+
+    }
+    protected void Button_Handicraft_Click(object sender, EventArgs e)
+    {
+        String skill = "Handicraft";
+        findAdvertiserfrom_AdvertisersDatabase(skill);
+    }
+    protected void Button_Farming_Click(object sender, EventArgs e)
+    {
+        String skill = "Farming";
+        findAdvertiserfrom_AdvertisersDatabase(skill);
+    }
+    protected void Button_Teaching_Click(object sender, EventArgs e)
+    {
+        String skill = "Teaching";
+        findAdvertiserfrom_AdvertisersDatabase(skill);
+    }
+    protected void Button_Weaving_Click(object sender, EventArgs e)
+    {
+        String skill = "Weaving";
+        findAdvertiserfrom_AdvertisersDatabase(skill);
+    }
+    protected void findAdvertiserfrom_AdvertisersDatabase(String skill)
+    {
+        SqlDataReader rdr = null;
+
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Job_Registration_ConnectionString"].ConnectionString);
+        try
+        {
+            //String choose_query = "Select Username FROM Skills where skill = @Skill";
+            String select_query = "SELECT * FROM Advertisers  where Skills LIKE @Skill";
+            conn.Open();
+            SqlCommand comm = new SqlCommand(select_query, conn);
+            //THIS IS VERY IMPORTANT : HOW TO USE PARAMETRIZED QUERY WITH LIKE
+            comm.Parameters.AddWithValue("@Skill", "%" + skill + "%");
+            GridView_Advertisements.DataSource = comm.ExecuteReader();
+            GridView_Advertisements.DataBind();
+
+        }
+        catch (Exception insertError)
+        {
+            Response.Write(insertError.ToString());
+        }
+        finally
+        {
+
+            if (rdr != null)
+                rdr.Close();
+            if (conn != null)
+                conn.Close();
+
+        }
+    }
+}
